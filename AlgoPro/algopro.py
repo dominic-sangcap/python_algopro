31dd40694da643e4ddb76351f1af7918ccd9c4de
Dominic <dominic.sangcap@gmail.com>
2021-06-26 20:59:46 -0700

    Valid Binary Search Tree Algorithm

--- AlgoPro/algopro.py
+++ AlgoPro/algopro.py
@@ -5,3 +5,17 @@ class Node(object):
        self.val = val
        self.left = left
        self.right = right
+
+class Solution(object):
+    def _isValidBSTHelper(self, n, low, high):
+        if not n:
+            return True
+        val = n.val
+        if ((val > low and val < high) and 
+            self._isValidBSTHelper(n.left, low, n.val) and 
+            self._isValidBSTHelper(n.right, n.val, high)):
+            return True
+        return False
+
+    def isValidBST(self, n):
+        return self._isValidBSTHelper(n, float('-inf'), float('inf'))
