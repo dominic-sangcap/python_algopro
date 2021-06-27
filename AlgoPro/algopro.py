73ba6a295a42097989807fb0b775e4a69e1f50f2
Dominic <dominic.sangcap@gmail.com>
2021-06-26 20:09:23 -0700

    changed README.me and added files

---
+++ AlgoPro/algopro.py
@@ -0,0 +1,7 @@
+#Lesson 1
+#declare node
+class Node(object):
+    def __init__(self, val, left=None, right=None):
+       self.val = val
+       self.left = left
+       self.right = right
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python_algopro
+# Testing git clone and other stuff and adding folder to repo
