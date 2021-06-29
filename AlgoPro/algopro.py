e580ff45f44aa0202100ffadc57a0b73645698b9
Dominic <dominic.sangcap@gmail.com>
2021-06-28 20:32:07 -0700

    Lesson 3 Add numbers in linked list

--- AlgoPro/algopro.py
+++ AlgoPro/algopro.py
@@ -45,3 +45,38 @@ class Solution(object):
 # where, [] is an array of char defining what is available, and second parameter asking what word can be spelled
 # shud be True
 print(Solution().canSpell(['a', 'b', 'c', 'd', 'e', 'f'], 'bed'))
+
+#Lesson 3
+class Node(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    #iteratively
+    def addTwoNumbers(self, l1, l2):
+        return self.addTwoNumbersIterative(l1, l2)
+
+    def addTwoNumbersIterative(self, l1, l2):   
+        a = l1 
+        b = l2
+        c = 0
+        ret = current = None
+
+        #loop though while either list has a value
+        while a or b:
+            val = a.val + b.val + c
+            c = val / 10
+            if not current:
+                ret = current = Node(val % 10)
+            else:
+                current.next = Node(val % 10)
+                current = current.next
+            if a.next or b.next:
+                if not a.next:
+                    a.next = Node(0)
+                if not b.next:
+                    b.next = Node(0)
+            a = a.next
+            b = b.next
+        return ret
