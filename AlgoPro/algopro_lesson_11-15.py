0d8f352f8fe8169f0414143329582efc720a4386
Dominic <dominic.sangcap@gmail.com>
2021-07-10 14:09:32 -0700

    AlgoPro Lesson 15 Simple Calculator

--- AlgoPro/algopro_lesson_11-15.py
+++ AlgoPro/algopro_lesson_11-15.py
@@ -142,3 +142,32 @@ class Solution(object):
 #test input
 print(Solution().pushDominoes('..R...L..R.'))
 
+#Lesson 15
+#Simple Calculator
+class Solution(object):
+    def __eval_helper(self, expression, index):
+        op = '+'
+        result = 0
+        while index < len(expression):
+            char = expression[index]
+            if char in ('-', '+'):
+                op = char
+            else:
+                value = 0
+                if char.isdigit():
+                    value = int(char)
+                elif char == '(':
+                    (value, index) = self.__eval_helper(expression, index + 1)
+                if op == '-':
+                    result -= value
+                if op == '+':
+                    result += value
+
+            index += 1
+        return (result, index)
+    
+    def eval(self, expression):
+        return self.__eval_helper(expression, 0)[0]
+
+#test input
+print(Solution().eval('- (3 + ( 2 - 1) )'))
