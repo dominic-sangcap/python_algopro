1c7ee3cc810697c6c48e4b51a0b5cfc891b1201c
Dominic <dominic.sangcap@gmail.com>
2021-07-15 10:07:22 -0700

    Algopro Lesson 20 remove kth element from linked list

--- AlgoPro/algopro_lesson_16-20.py
+++ AlgoPro/algopro_lesson_16-20.py
@@ -104,3 +104,39 @@ class Solution(object):
             result.append(heapq.heappop(heap)[1])
         return result
 print(Solution().topKFrequent([1,1,1,2,2,3,], 2))
+
+#lesson 20
+#Remove kth element from linked list
+class Node:
+    def __init__(self, val, next):
+        self.val = val
+        self.next = next
+    def __str__(self):
+        n = self
+        answer = ''
+        while n:
+            answer += str(n.val)
+            n = n.next
+        return answer
+
+def remove_kth_element(node, k):
+    slow, fast = node, node
+    for i in range(k):
+        fast = fast.next
+    #edge cases
+    if not fast:
+        return node.next
+
+    prev = None
+    while fast:
+        prev = slow
+        fast = fast.next
+        slow = slow.next
+    prev.next = slow.next 
+    return node 
+#test input
+head = Node(1, Node(2, Node(3, Node(4, Node(5, None)))))
+print(head)
+#should remove 3
+remove_kth_element(head, 3)
+print(head)
