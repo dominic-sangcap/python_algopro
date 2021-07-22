f173782bd86bf279b2278d79c34ba8fb7db1f796
Dominic <dominic.sangcap@gmail.com>
2021-07-22 08:53:19 -0700

    AlgoPro lesson 27 remove zero sum nodes

--- AlgoPro/algopro_lesson_26-30.py
+++ AlgoPro/algopro_lesson_26-30.py
@@ -28,3 +28,53 @@ print(q.dequeue())
 print(q.dequeue())
 print(q.dequeue())
 print(q.dequeue())
+
+#Lesson 27
+#Remove Zero Sum NOdes
+import collections
+
+class Node(object):
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        n = self
+        ret = ''
+        while n:
+            ret += str(n.val) + ' '
+            n = n.next
+        return ret
+
+class Solution(object):
+    def removeZeroSumSublists(self, node):
+        sumToNode = collections.OrderedDict()
+        sum = 0
+        dummy = Node(0)
+        dummy.next = node
+        n = dummy
+        while n:
+            sum += n.val   
+            if sum not in sumToNode:
+                sumToNode[sum] = n
+            else:
+                prev = sumToNode[sum]
+                prev.next = n.next
+                while list(sumToNode.keys())[-1] != sum:
+                    sumToNode.popitem()
+            n = n.next           
+        return dummy.next
+
+# File "c:\Users\Dom\Desktop\python_algopro\AlgoPro\tempCodeRunnerFile.py", line 30, in removeZeroSumSublists
+#     while sumToNode.keys()[-1] != sum:
+# TypeError: 'odict_keys' object is not subscriptable
+# -----means turn object into a list
+#test input
+n = Node(3)
+n.next = Node(1)
+n.next.next = Node(2)
+n.next.next.next = Node(-1)
+n.next.next.next.next = Node(-2)
+n.next.next.next.next.next = Node(4)
+n.next.next.next.next.next.next = Node(1)
+print(Solution().removeZeroSumSublists(n))
