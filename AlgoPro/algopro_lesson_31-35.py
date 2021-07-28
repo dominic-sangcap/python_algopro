9648e11507e566fae0026f485aa83dcdad8212c0
Dominic <dominic.sangcap@gmail.com>
2021-07-28 09:50:17 -0700

    algopro lesson 33 meting rooms

--- AlgoPro/algopro_lesson_31-35.py
+++ AlgoPro/algopro_lesson_31-35.py
@@ -57,3 +57,24 @@ class Solution(object):
         return -1
 #test input
 print(Solution().first_missing_position([3, 4, -1, 1]))
+
+#Lesson 33
+#Meeting Rooms
+import heapq
+
+def meeting_rooms(meetings):
+    meetings.sort(key=lambda x: x[0])
+    meeting_ends = []
+    max_rooms = 0
+
+    for meeting in meetings:
+        while meeting_ends and meeting_ends[0] <= meeting[0]:
+            heapq.heappop(meeting_ends)
+        heapq.heappush(meeting_ends, meeting[1])
+        max_rooms = max(max_rooms, len(meeting_ends))
+
+    return max_rooms
+#test input
+print(meeting_rooms([[0, 10], [10, 20]]))
+
+print(meeting_rooms([[20, 30], [10, 21], [0, 50]]))
