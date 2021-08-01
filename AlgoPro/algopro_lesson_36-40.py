0ee2322cdf929c0a22f63814cb5256d1cacdcd47
Dominic <dominic.sangcap@gmail.com>
2021-08-01 08:54:56 -0700

    algopro lesson 37 balanced binary tree

--- AlgoPro/algopro_lesson_36-40.py
+++ AlgoPro/algopro_lesson_36-40.py
@@ -26,3 +26,36 @@ node.left.right = Node(5)
 print(valuesAtLevel(node, 3))
 print(valuesAtLevel(node, 2))
 print(valuesAtLevel(node, 1))
+
+#Lesson 37
+#balanced binary tree
+class Node(object):
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    #returns True/False, and depth of node
+    def _is_balanced_helper(self, n):
+        if not n:
+            return (True, 0)
+        
+        lBalanced, lHeight = self._is_balanced_helper(n.left)
+        rBalanced, rHeight = self._is_balanced_helper(n.right)
+        return (lBalanced and rBalanced and abs(lHeight - rHeight) <= 1, 
+            max(lHeight, rHeight) + 1)
+
+
+    def isBalanced(self, n):
+        return self._is_balanced_helper(n)[0]
+
+#test input
+n = Node(1)
+n.left = Node(2)
+n.left.left = Node(4)
+n.right = Node(3)
+print(Solution().isBalanced(n))
+
+n.right = None
+print(Solution().isBalanced(n))
