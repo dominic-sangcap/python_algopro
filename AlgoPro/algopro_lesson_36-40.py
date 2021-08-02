4c5aca4930087782bbb45191bc5742ece15b86cb
Dominic <dominic.sangcap@gmail.com>
2021-08-02 08:58:13 -0700

    algopro lesson 38 count of unival subtrees

--- AlgoPro/algopro_lesson_36-40.py
+++ AlgoPro/algopro_lesson_36-40.py
@@ -59,3 +59,45 @@ print(Solution().isBalanced(n))
 
 n.right = None
 print(Solution().isBalanced(n))
+
+#Lesson 38
+#Count of Unival Subtrees
+class Node(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def count_unival_subtrees(node):
+    count, is_unival = count_unival_subtrees_helper(node)
+    return count
+
+def count_unival_subtrees_helper(node):
+    if not node:
+        return 0, True
+    
+    left_count, is_left_unival = count_unival_subtrees_helper(node.left)
+    right_count, is_right_unival = count_unival_subtrees_helper(node.right)
+
+    if is_left_unival and is_right_unival and (not node.left or node.val == node.left.val) and (not node.right or node.val == node.right.val):
+        return left_count + right_count + 1, True
+    
+    return left_count + right_count, False
+
+#test input
+#    0
+#   / \
+#  1   0
+#     / \
+#    1   0
+#   / \
+#  1   1
+a = Node(0)
+a.left = Node(1)
+a.right = Node(0)
+a.right.left = Node(1)
+a.right.right = Node(0)
+a.right.left.left = Node(1)
+a.right.left.right = Node(1)
+
+print(count_unival_subtrees(a))
