47b2cc986ae99fe7c304fc51ca17e23a33a41a98
Dominic <dominic.sangcap@gmail.com>
2021-08-04 08:28:31 -0700

    algopro lesson 40 group word anagrams

--- AlgoPro/algopro_lesson_36-40.py
+++ AlgoPro/algopro_lesson_36-40.py
@@ -135,3 +135,29 @@ n.left.left = Node(4)
 print(Solution().maxDepth(n))
 
 print(Solution().maxDepthRecursive(n))
+
+#lesson 40
+#Group words anagrams
+import collections
+
+
+def hashkey(str):
+    return "".join(sorted(str))
+
+
+def hashkey2(str):
+    arr = [0] * 26
+    for c in str:
+        arr[ord(c) - ord('a')] = 1
+    return tuple(arr)
+
+def groupAnagramsWords(strs):
+    groups = collections.defaultdict(list)
+    for s in strs:
+        groups[hashkey2(s)].append(s)
+    return groups.values()
+
+
+
+#test input
+print(groupAnagramsWords(['abc', 'bcd', 'cba', 'cbd', 'efg']))
