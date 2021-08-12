8923c7ecabff8a5af4c51ee2e060432408482f78
Dominic <dominic.sangcap@gmail.com>
2021-08-11 17:06:16 -0700

    algopro lesson 47 climbing stairs

--- AlgoPro/algopro_lesson_46-50.py
+++ AlgoPro/algopro_lesson_46-50.py
@@ -10,3 +10,27 @@ def calcAngle(h, m):
 #test input 
 print(calcAngle(3, 15))
 print(calcAngle(3, 00))
+
+#Lesson 47
+#climbing stairs
+#recursive implementation
+def staircase(n):
+    if n <= 1:
+        return 1
+    return staircase(n - 1) + staircase(n - 2)
+
+def staircase2(n):
+    prev = 1
+    prevprev = 1
+    curr = 0
+    for i in range(2, n + 1):
+        curr = prev + prevprev
+
+        prevprev = prev
+        prev = curr
+    return curr
+
+#test input
+print(staircase2(5))
+
+print(staircase(5))
