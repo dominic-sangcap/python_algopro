93832e764021ec70bddcb80b29a1a5f381290c39
Dominic <dominic.sangcap@gmail.com>
2021-08-17 09:53:48 -0700

    algopro lesson 53 phone numbers

--- AlgoPro/algopro_lesson_51-55.py
+++ AlgoPro/algopro_lesson_51-55.py
@@ -36,3 +36,41 @@ def buy_and_sell2(arr):
     return max_profit
 
 print(buy_and_sell2([9, 11, 8, 5, 7, 10]))
+
+#Leson 53
+#Phone Numbers
+lettersMaps = {
+    1: [],
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+    0: []
+}
+validwords = ['dog', 'fish', 'cat', 'fog']
+
+def makeWords_helper(digits, letters):
+    if not digits:
+        word = ''.join(letters)
+        if word in validwords:
+            return [word]
+        return []
+
+    results = []
+    chars = lettersMaps[digits[0]]
+    for char in chars:
+        results += makeWords_helper(digits[1:], letters + [char])
+    return results
+
+def makeWords(phone):
+    digits = []
+    for digit in phone:
+        digits.append(int(digit))
+    return makeWords_helper(digits, [])
+
+#test input
+print(makeWords('364'))
