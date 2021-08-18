7e1b6d14872307437ddc221510d39bd69940ad54
Dominic <dominic.sangcap@gmail.com>
2021-08-18 15:13:27 -0700

    algopro lesson 54 quicselect

--- AlgoPro/algopro_lesson_51-55.py
+++ AlgoPro/algopro_lesson_51-55.py
@@ -74,3 +74,67 @@ def makeWords(phone):
 
 #test input
 print(makeWords('364'))
+
+#Lesson 54
+#QuickSelect
+import heapq
+
+#brute force
+def findKthLargest(arr, k):
+    for i in range(0, k):
+        (max_value, max_index) = (arr[0], 0)
+        for j in range(0, len(arr)):
+            if max_value < arr[j]:
+                (max_value, max_index) = arr[j], j
+        arr = arr[:max_index] + arr[max_index + 1:]
+    for j in range(0, len(arr)):
+            if max_value < arr[j]:
+                (max_value, max_index) = arr[j], j
+    return max_value
+
+#nlogn time
+def findKthLargest2(arr, k):
+    return sorted(arr)[-k]
+
+#heap implementation
+def findKthLargest3(arr, k):
+    arr = list(map(lambda x: -x, arr))
+    heapq.heapify(arr)
+    for i in range(0, k - 1):
+        heapq.heappop(arr)
+    return -arr[0]
+
+#quicslect implmentation
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[i], arr[high] = arr[high], arr[i]
+    return i
+
+def quickselect(arr, k):
+    k = len(arr) - k
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        pivotIndex = partition(arr, left, right)
+        if pivotIndex == k:
+            return arr[pivotIndex]
+        elif pivotIndex > k:
+            right = pivotIndex - 1
+        else:
+            left = pivotIndex + 1
+    return -1
+
+#test input
+print(quickselect([8, 7, 2, 3, 4, 1, 5, 6, 9, 0], 4))
+
+print(findKthLargest3([8, 7, 2, 3, 4, 1, 5, 6, 9, 0], 4))
+
+print(findKthLargest2([8, 7, 2, 3, 4, 1, 5, 6, 9, 0], 4))
+
+print(findKthLargest([8, 7, 2, 3, 4, 1, 5, 6, 9, 0], 4))
