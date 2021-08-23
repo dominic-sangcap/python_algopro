ecee637d39d57f77c6f155d83de11dfa26cebcdb
Dominic <dominic.sangcap@gmail.com>
2021-08-23 08:48:03 -0700

    algopro lesson 59 autocompletion

--- algopro_lesson_56-60.py
+++ algopro_lesson_56-60.py
@@ -111,3 +111,56 @@ def findClosestPoints2(points, k):
 print (findClosestPoints2([[1, 1], [3, 3], [2, 2], [4, 4], [-1, -1]], 3))
 
 print (findClosestPoints([[1, 1], [3, 3], [2, 2], [4, 4], [-1, -1]], 3))
+
+#lesson 59
+#autocompletion
+class Node(object):
+    def __init__(self, isWord, children):
+        self.isWord = isWord
+        #will be a dictionary, a:..., b:....
+        self.children = children
+
+class Solution:
+    def build(self, words):
+        trie = Node(False, {})
+        for word in words:
+            current = trie
+            for char in word:
+                if not char in current.children:
+                    current.children[char] = Node(False, {})
+                current = current.children[char]
+            current.isWord = True
+        self.trie = trie
+
+    def autocomplete(self, word):
+        current = self.trie
+        for char in word:
+            if not char in current.children:
+                return []
+            current = current.children[char]
+        
+        words = []
+        #self.dfs(current, word, words)
+        self.dfsIterative(current, word, words)
+        return words
+    
+    def dfs(self, node, prefix, words):
+        if node.isWord:
+            words.append(prefix)
+        for char in node.children:
+            self.dfs(node.children[char], prefix + char, words)
+
+    def dfsIterative(self, node, prefix, words):
+        stack = [(node, prefix)]
+        while len(stack):
+            (node, prefix) = stack.pop()
+            if node.isWord:
+                words.append(prefix)
+            for char in node.children:
+                child = node.children[char]
+                stack.append((child, prefix + char))
+
+#test input
+s = Solution()
+s.build(['dog', 'dark', 'cat', 'door', 'dodge'])
+print(s.autocomplete('do'))
