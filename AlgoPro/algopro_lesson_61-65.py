22abea3316855ef7a9d7f95a93d71f0a383c3ca3
Dominic <dominic.sangcap@gmail.com>
2021-08-28 09:47:41 -0700

    algopro lesson 64 consecutive bit ones

--- AlgoPro/algopro_lesson_61-65.py
+++ AlgoPro/algopro_lesson_61-65.py
@@ -74,3 +74,24 @@ def clean_path(path):
 #test input
 path = '/users/tech/docs/.././desk/../'
 print(clean_path(path))
+
+#Lesson 64
+#Consecutive Bit Ones
+def longest_run(n):
+    longest_run = 0
+    current_run = 0
+    BITMASK = 1
+
+    while n != 0:
+        if n & BITMASK == 0:
+            longest_run = max(longest_run, current_run)
+            current_run = 0
+        else:
+            current_run += 1
+        n = n >> 1
+    longest_run = max(longest_run, current_run)
+    return longest_run
+
+#test input
+n = 242
+print(longest_run(n))
