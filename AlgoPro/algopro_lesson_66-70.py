d967bc49198c4319892079b21dc9fc67770d4aae
Dominic <dominic.sangcap@gmail.com>
2021-08-30 10:47:10 -0700

    changing input

---
+++ AlgoPro/algopro_lesson_66-70.py
@@ -0,0 +1,24 @@
+#Lesson 66
+#Check for palindromes
+from collections import defaultdict
+
+def find_palindrome(str):
+    char_counts = defaultdict(int)
+    for c in str:
+        char_counts[c] += 1
+
+    pal = ''
+    odd_char = ''
+    for c, cnt in char_counts.items():
+        if cnt % 2 == 0:
+            pal += c * (cnt//2)
+        elif odd_char == '':
+            odd_char = c
+            pal += c * (cnt//2)
+        else:
+            return False
+    
+    return pal + odd_char + pal[::-1]
+
+#test input
+print(find_palindrome('fofxo'))   
