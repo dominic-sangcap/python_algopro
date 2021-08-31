74382c6e4403fc312089c1c1371ed28e9bf02eb7
Dominic <dominic.sangcap@gmail.com>
2021-08-31 12:10:24 -0700

    algopro lesson 67 rectangle intersection

--- AlgoPro/algopro_lesson_66-70.py
+++ AlgoPro/algopro_lesson_66-70.py
@@ -14,7 +14,7 @@ def find_palindrome(str):
             pal += c * (cnt//2)
         elif odd_char == '':
             odd_char = c
-            pal += c * (cnt//2)
+            pal += c * (cnt//2) #unsure if this is actually needed
         else:
             return False
     
@@ -22,3 +22,32 @@ def find_palindrome(str):
 
 #test input
 print(find_palindrome('fofxo'))   
+
+#Lesson 67
+class Rectangle(object):
+    def __init__(self, min_x=0, min_y=0, max_x=0, max_y=0):
+        self.min_x = min_x
+        self.min_y = min_y
+        self.max_x = max_x
+        self.max_y = max_y
+
+    def area(self):
+        if self.min_x >= self.max_x:
+            return 0
+        if self.min_y >= self.max_y:
+            return 0
+        return (self.max_x - self.min_x) * (self.max_y - self.min_y) 
+
+def intersect_rect(a, b):
+    return Rectangle(
+        max(a.min_x, b.min_x),
+        max(a.min_y, b.min_y),
+        min(a.max_x, b.max_x),
+        min(a.max_y, b.max_y)
+    )
+
+#test input
+a = Rectangle(0, 0, 3, 2)
+b = Rectangle(1, 1, 3, 3)
+intersesction = intersect_rect(a, b)
+print(intersesction.area())
