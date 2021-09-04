007a560a14c231f3f1f1df21757257a46f0fb20d
Dominic <dominic.sangcap@gmail.com>
2021-09-04 12:29:18 -0700

    algo pro lesson 69 determine if number

--- AlgoPro/algopro_lesson_66-70.py
+++ AlgoPro/algopro_lesson_66-70.py
@@ -101,3 +101,57 @@ b.right = Node(2)
 print(find_subtree(n, b))
 
 print(find_subtree2(n, b))
+
+#Lesson 69
+#Determine if Number
+
+from enum import Enum
+class DigitState(Enum):
+    BEGIN = 0
+    NEGATIVE1 = 1
+    DIGIT1 = 2
+    DOT = 3
+    DIGIT2 = 4
+    E = 5
+    NEGATIVE2 = 6
+    DIGIT3 =  7
+
+STATE_VALIDATOR = {
+    DigitState.BEGIN: lambda x: True,
+    DigitState.DIGIT1: lambda x: x.isdigit(),
+    DigitState.NEGATIVE1: lambda x: x == '-',
+    DigitState.DIGIT2: lambda x: x.isdigit(),
+    DigitState.DOT: lambda x: x == '.',
+    DigitState.E: lambda x: x == 'e',
+    DigitState.NEGATIVE2: lambda x: x == '-',
+    DigitState.DIGIT3: lambda x: x.isdigit(),
+}
+
+NEXT_STATES_MAP = {
+    DigitState.BEGIN: [DigitState.NEGATIVE1, DigitState.DIGIT1],
+    DigitState.NEGATIVE1: [DigitState.DIGIT1, DigitState.DOT],
+    DigitState.DIGIT1: [DigitState.DIGIT1, DigitState.DOT, DigitState.E],
+    DigitState.DOT: [DigitState.DIGIT2],
+    DigitState.DIGIT2: [DigitState.DIGIT2, DigitState.E],
+    DigitState.NEGATIVE2: [DigitState.DIGIT3],
+    DigitState.DIGIT3: [DigitState.DIGIT3],
+}
+
+def parse_number(str):
+    state = DigitState.BEGIN
+
+    for c in str:
+        found = False
+        for next_State in NEXT_STATES_MAP[state]:
+            if STATE_VALIDATOR[next_State](c):
+                state = next_State
+                found = True
+                break
+        if not found:
+            return False
+    return state in [DigitState.DIGIT1, DigitState.DIGIT2, DigitState.DIGIT3]
+
+#test input
+print(parse_number('12.3'))
+
+print(parse_number('12a'))
