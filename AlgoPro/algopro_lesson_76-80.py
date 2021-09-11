7cc742050b5d38aa269c9038b7e603d9925aafcd
Dominic <dominic.sangcap@gmail.com>
2021-09-11 13:38:14 -0700

    algopro lesson 76 string to int

---
+++ AlgoPro/algopro_lesson_76-80.py
@@ -0,0 +1,19 @@
+#Lesson 76
+#string to int
+def convert_to_int(str):
+  is_negative = False
+  start_index = 0
+  if str[0] == '-':
+    is_negative = True
+    start_index = 1
+
+  result = 0
+  for c in str[start_index:]:
+    result = result * 10 + ord(c) - ord('0')
+
+  if is_negative:
+    result *= -1
+  return result
+
+#test input
+print(convert_to_int('-105'))
