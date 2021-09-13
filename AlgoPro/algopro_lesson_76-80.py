11ea9e24cfc5f5c79808606e36eb062554336fc7
Dominic <dominic.sangcap@gmail.com>
2021-09-13 11:24:31 -0700

    algopro lesson 77 shortest unique prefix

--- AlgoPro/algopro_lesson_76-80.py
+++ AlgoPro/algopro_lesson_76-80.py
@@ -17,3 +17,51 @@ def convert_to_int(str):
 
 #test input
 print(convert_to_int('-105'))
+
+#Lesson 77
+#Shortest Unique Prefix
+class Node:
+  def __init__(self):
+    self.count = 0
+    self.children = {}
+  
+class Trie:
+  def __init__(self):
+    self.root = Node()
+
+  def insert(self, word):
+    node = self.root
+
+    for c in word:
+      if c not in node.children:
+        node.children[c] = Node()
+      node = node.children[c]
+      node.count = node.count + 1
+  
+  def unique_prefix(self, word):
+    node = self.root
+    prefix = ''
+
+    for c in word:
+      if node.count == 1:
+        return prefix
+      else:
+        node = node.children[c]
+        prefix += c
+    return prefix
+
+def shortest_unique_prefix(words):
+  trie = Trie()
+
+  for word in words:
+    trie.insert(word)
+  
+  unique_prefixes_list = []
+  for word in words:
+    unique_prefixes_list.append(trie.unique_prefix(word))
+
+
+  return unique_prefixes_list
+
+#test input
+print(shortest_unique_prefix(['jon', 'john', 'jack', 'tech']))
