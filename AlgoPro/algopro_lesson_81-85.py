e57ac05f523793baa815a3c34f3eac9a8f25498e
Dominic <dominic.sangcap@gmail.com>
2021-09-18 15:47:57 -0700

    algopro lesson 82 multitasking

--- AlgoPro/algopro_lesson_81-85.py
+++ AlgoPro/algopro_lesson_81-85.py
@@ -19,3 +19,21 @@ def swap_every_two(node):
 #test input
 node = Node(1, Node(2, Node(3, Node(4, Node(5)))))
 print(swap_every_two(node))
+
+#lesson 82
+#multitasking
+def findtime(tasks, cooldown):
+  lastpos = {}
+  current = 0
+
+  for task in tasks:
+    if task in lastpos:
+      if current - lastpos[task] <= cooldown:
+        current = cooldown + lastpos[task] + 1
+    lastpos[task] = current
+    current += 1
+    print(lastpos)
+  return current
+
+#test input
+print(findtime([1, 1, 2, 1], 2))
