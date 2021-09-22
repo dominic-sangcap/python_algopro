d25bebdd45100095c3f63ea24bd7a2b02ad00a94
Dominic <dominic.sangcap@gmail.com>
2021-09-21 19:44:30 -0700

    algopro lesson 83 generate binary search trees

--- AlgoPro/algopro_lesson_81-85.py
+++ AlgoPro/algopro_lesson_81-85.py
@@ -37,3 +37,43 @@ def findtime(tasks, cooldown):
 
 #test input
 print(findtime([1, 1, 2, 1], 2))
+
+#Lesson 83
+#generate binary search trees
+class Node:
+  def __init__(self, value, left=None, right=None):
+    self.value = value
+    self.left = left
+    self.right = right
+
+  def __repr__(self):
+    result = str(self.value)
+    if self.left:
+      result = result + str(self.left)
+    if self.right:
+      result = result + str(self.right)
+    return result
+
+def gen_tree(nums):
+  if len(nums) == 0:
+    return [ None ]
+  if len(nums) == 1:
+    return [ Node(nums[0]) ]
+
+  bsts = []
+  for n in nums:
+    lefts = gen_tree(range(nums[0], n))
+    rights = gen_tree(range(n + 1, nums[-1] + 1))
+
+    for left in lefts:
+      for right in rights:
+        tree = Node(n, left, right)
+        bsts.append(tree)
+
+  return bsts
+
+def generate_bst(n):
+  return gen_tree(range(1, n + 1))
+
+#test input
+print(generate_bst(3))
