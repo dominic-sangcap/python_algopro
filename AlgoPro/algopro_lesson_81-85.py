188eadc4d2227f35e813f533f64dfef8fba91dbf
Dominic <dominic.sangcap@gmail.com>
2021-09-23 10:50:44 -0700

    algopro lesson 85 balanced binary trees

--- AlgoPro/algopro_lesson_81-85.py
+++ AlgoPro/algopro_lesson_81-85.py
@@ -125,3 +125,53 @@ n2 = Node(2, n4, n5)
 n1 = Node(1, n2, n3)
 
 print(zigzag_order(n1))
+
+#Lesson 85
+#balanced binary trees
+class Node:
+  def __init__(self, value, left=None, right=None):
+    self.value = value
+    self.left = left
+    self.right = right
+
+
+def tree_height(node):
+  if node is None:
+    return 0
+
+  heightLeft = tree_height(node.left)
+  heightRight = tree_height(node.right)
+
+  if heightLeft >= 0 and heightRight >= 0 and abs(heightLeft - heightRight) <= 1:
+    return max(heightLeft, heightRight) + 1
+  return -1
+
+
+def is_tree_balanced(node):
+  return tree_height(node) != -1
+
+#teset input
+n4 = Node(4)
+n3 = Node(3)
+n2 = Node(2, n4)
+n1 = Node(1, n2, n3)
+
+#      1
+#     / \
+#    2   3
+#   /
+#  4
+print(is_tree_balanced(n1))
+# True
+
+n4 = Node(4)
+n2 = Node(2, n4)
+n1 = Node(1, n2, None)
+
+#      1
+#     /
+#    2
+#   /
+#  4
+print(is_tree_balanced(n1))
+# False
